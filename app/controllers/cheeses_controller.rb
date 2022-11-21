0776dfecccbbdfcb528ab990750292a7e93420be
Kevin Sasse <sassek70@gmail.com>
2022-11-21 13:37:02 -0500

    commit tree 6684493bc93c0e7c332539c85e9fd0f47993ac16

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,34 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    
+    # only include certain keys
+    # render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+
+    # alternatively use except: to exclude certain fields
+    render json: cheeses, except: [:created_at, :updated_at]
   end
 
   # GET /cheeses/:id
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: {
+  #     id: cheese.id,
+  #     name: cheese.name,
+  #     price: cheese.price,
+  #     is_best_seller: cheese.is_best_seller
+  #   }
+  # end
+  
+
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese,  methods: [:summary], except: [:created_at, :updated_at]
+    else
+      # status: :not_found will produce a 404 status code
+      render json: {error: 'Cheese not found'}, status: :not_found
+    end
   end
-
+  
 end
